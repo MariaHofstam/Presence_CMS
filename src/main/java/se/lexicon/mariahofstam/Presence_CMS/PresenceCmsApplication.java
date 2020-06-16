26119d2dce5c6b99a8e8e8f72f6788e1720d3672
Maria <45755355+MariaHofstam@users.noreply.github.com>
2020-06-16 12:45:05 +0200

    Second commit

---
+++ src/main/java/se/lexicon/mariahofstam/Presence_CMS/PresenceCmsApplication.java
@@ -0,0 +1,17 @@
+package se.lexicon.mariahofstam.Presence_CMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceCmsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PresenceCmsApplication.class, args);
+
+		System.out.println("SYSTEM STARTED!");
+	}
+
+
+}
---
+++ src/test/java/se/lexicon/mariahofstam/Presence_CMS/PresenceCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.mariahofstam.Presence_CMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
