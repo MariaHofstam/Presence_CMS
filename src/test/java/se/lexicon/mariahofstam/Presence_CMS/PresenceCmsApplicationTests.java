2e669e8ed72641304aa88c36a4e95c1e0f008b88
Anton Edholm <50101559+Draggel12345@users.noreply.github.com>
2020-09-23 14:27:30 +0200

    Changed to junit4

--- src/test/java/se/lexicon/mariahofstam/Presence_CMS/PresenceCmsApplicationTests.java
+++ src/test/java/se/lexicon/mariahofstam/Presence_CMS/PresenceCmsApplicationTests.java
@@ -1,8 +1,11 @@
 package se.lexicon.mariahofstam.Presence_CMS;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PresenceCmsApplicationTests {
 
